2cd6d34171f11d95049c5b0ead6fc1ba09358ce4
AlexanderMcLaughlin <alexander.r.mclaughlin@gmail.com>
2019-12-04 22:12:33 -0500

    Removing Unnecessary Class

--- src/buildingblocks/Blocks.java
+++ src/buildingblocks/Blocks.java
@@ -28,6 +28,7 @@ public class Blocks extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("GUI");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
